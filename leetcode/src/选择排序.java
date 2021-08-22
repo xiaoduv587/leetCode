006e87a24a386d910cc178883ae673175bc1ffdf
zhibindu1 <zhibindu1@lenovo.com>
2021-08-22 19:52:26 +0800

    添加选择排序的注释

--- leetcode/src/选择排序.java
+++ leetcode/src/选择排序.java
@@ -4,34 +4,34 @@ public class 选择排序 {
 
 
     private static void selectSort(int[] a) {
-        if (a == null || a.length == 0) {
+        if (a == null || a.length < 2) {
             return;
         }
 
         int length = a.length;
-        for (int i = 0; i < length; i++) {
+        for (int i = 0; i < length - 1; i++) {  //i ~ N-1
+
+            //最小值在哪个位置上 i~N-1
             int minIndex = i;
 
             // 查找第（i+1）个最小元素，放到下标为i的位置
-            for (int j = i + 1; j < length; j++) {
-                if (a[ j ] < a[ minIndex ]) {
-                    minIndex = j;
-                }
+            for (int j = i + 1; j < length; j++) { // i ~ N-1上找最小值的下标
+                minIndex = a[ j ] < a[ minIndex ] ? j : minIndex;
             }
-
             // 如果minIndex的值变了，说明有更小的元素，则a[minIndex]和a[i]进行交换
-            if (minIndex != i) {
-                int temp = a[ minIndex ];
-                a[minIndex]=a[i];
-                a[i]=temp;
-            }
+            swap(a, i, minIndex);
         }
-
 
     }
 
+    private static void swap(int[] a, int i, int minIndex) {
+        int temp = a[ minIndex ];
+        a[ minIndex ] = a[ i ];
+        a[ i ] = temp;
+    }
+
     public static void main(String[] args) {
-        int a[] = {49, 38, 65, 97, 76, 13, 27, 49};
+        int a[] = {49, 38, 65, 97, 76, 13, 27, 27, 49};
         selectSort(a);
         System.out.println(Arrays.toString(a));
     }
